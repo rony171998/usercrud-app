e5b19026a851abb54bce9c2d2fac70b1ccbeb35c
rony171998 <rony171998@gmail.com>
2022-05-22 13:28:42 -0500

    v1.1

--- src/Componets/UserForm.js
+++ src/Componets/UserForm.js
@@ -1,12 +1,14 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./UserForm.css"
 
-const UserForm = ({addUser,userSelected,updateUser,deselectUser}) => {
-    const [first_name,setfirst_name]=useState("")
-    const [last_name,setlast_name]=useState("")
-    const [email,setemail]=useState("")
-    const [birthday,setbirthday]=useState("")
-    const [password,setpassword]=useState("")
+const UserForm = ({ addUser, userSelected, updateUser, deselectUser }) => {
+    const [first_name, setfirst_name] = useState("")
+    const [last_name, setlast_name] = useState("")
+    const [email, setemail] = useState("")
+    const [birthday, setbirthday] = useState("")
+    const [password, setpassword] = useState("")
+    const [titleform, setTitleForm] = useState("")
+
 
     useEffect(() => {
         if (userSelected !== null) {
@@ -15,13 +17,15 @@ const UserForm = ({addUser,userSelected,updateUser,deselectUser}) => {
             setemail(userSelected.email)
             setbirthday(userSelected.birthday)
             setpassword(userSelected.password)
-        }else {
+            setTitleForm("Edit")
+        } else {
+            setTitleForm("Add")
             reset();
         }
 
     }, [userSelected])
-    const submit=()=>{
-        const user ={
+    const submit = () => {
+        const user = {
             first_name: first_name,
             last_name: last_name,
             email: email,
@@ -29,81 +33,84 @@ const UserForm = ({addUser,userSelected,updateUser,deselectUser}) => {
             password: password
         }
         if (userSelected !== null) {
-            alert("actualizando a "+user.first_name);
             updateUser(user)
             deselectUser()
-        }else{
-            alert("registrando a "+user.first_name);
+        } else {
             addUser(user)
             reset()
         }
-        
-        }
 
-        const reset = () => {
-            setfirst_name("")
-            setlast_name("")
-            setemail("")
-            setbirthday("")
-            setpassword("")
-          };
+    }
+
+    const reset = () => {
+        setfirst_name("")
+        setlast_name("")
+        setemail("")
+        setbirthday("")
+        setpassword("")
+    };
 
     return (
         <form onSubmit={submit} className="userForm">
-            <h1>New User</h1>
+            <h1>{titleform} User</h1>
             <div>
                 <label htmlFor="first_name" className="form-label">
-                first_name
+                    first_name
                 </label>
-                <input 
-                type="text"
-                className='form-input'
-                id='first_name'
-                onChange={e=> setfirst_name(e.target.value)}
+                <input
+                    type="text"
+                    className='form-input'
+                    id='first_name'
+                    onChange={e => setfirst_name(e.target.value)}
+                    value={first_name}
                 />
             </div>
             <div>
                 <label htmlFor="last_name" className="form-label">
-                last_name
+                    last_name
                 </label>
-                <input 
-                type="text"
-                className='form-input'
-                id='last_name'
-                onChange={e=> setlast_name(e.target.value)}
+                <input
+                    type="text"
+                    className='form-input'
+                    id='last_name'
+                    onChange={e => setlast_name(e.target.value)}
+                    value={last_name}
                 />
             </div>
             <div>
                 <label htmlFor="email" className="form-label">
-                email
+                    email
                 </label>
-                <input 
-                type="text"
-                className='form-input'
-                id='email'
-                onChange={e=> setemail(e.target.value)}
+                <input
+                    type="text"
+                    className='form-input'
+                    id='email'
+                    onChange={e => setemail(e.target.value)}
+                    value={email}
                 />
             </div>
             <div>
                 <label htmlFor="birthday" className="form-label">
-                birthday
+                    birthday
                 </label>
-                <input 
-                type="date"
-                className='form-input'
-                id='birthday'
-                onChange={e=> setbirthday(e.target.value)}
+                <input
+                    type="date"
+                    className='form-input'
+                    id='birthday'
+                    onChange={e => setbirthday(e.target.value)}
+                    value={birthday}
                 />
             </div>
             <div>
                 <label htmlFor="password" className="form-label">
-                password
+                    password
                 </label>
-                <input 
-                type="password"
-                className='form-input'
-                id='password'
-                onChange={e=> setpassword(e.target.value)}
+                <input
+                    type="password"
+                    className='form-input'
+                    id='password'
+                    onChange={e => setpassword(e.target.value)}
+                    value={password}
                 />
             </div>
             <button>Submit</button>
